00eb5d1fa229050f3e9240f37c1db48bff980a63
Alisher Karim <alisherktlwik@gmail.com>
2023-10-15 20:47:27 +0900

    init backend with user model

--- ucsapi/asgi.py
+++ ucsapi/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for ucsbackend project.
+ASGI config for ucsapi project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ucsbackend.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ucsapi.settings')
 
 application = get_asgi_application()
--- ucsapi/wsgi.py
+++ ucsapi/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for ucsbackend project.
+WSGI config for ucsapi project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ucsbackend.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ucsapi.settings')
 
 application = get_wsgi_application()
